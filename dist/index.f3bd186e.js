2acb50360e93dbcbfd74c08269a11f358aa8cc40
EmAdler <eadl9050@uni.sydney.edu.au>
2022-05-02 15:12:28 +1000

    not really working task list

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("taskform");
 const button = document.querySelector("#taskform > button");
 let taskInput = document.getElementById("taskInput");
+let tasklist = document.getElementById("tasklist");
 let dueDateInput = document.getElementById("dueDateInput");
 let completionTimeInput = document.getElementById("completionTimeInput");
 let estimatedTimeInput = document.getElementById("estimatedTimeInput");
@@ -13,7 +14,7 @@ button.addEventListener("click", function(event) {
     addTask(task, dueDate, estimatedTime, completionTime, false);
     console.log(taskList);
 });
-let taskList = [];
+let taskListArray = [];
 function addTask(taskDescription, dueDate, estimatedTime, completionTime, completionStatus) {
     let d = new Date();
     let dateCreated = d.getFullYear();
@@ -25,8 +26,26 @@ function addTask(taskDescription, dueDate, estimatedTime, completionTime, comple
         completionTime,
         completionStatus
     };
-    taskList.push(task);
-    return task;
+    taskListArray.push(task);
+    renderTask(task);
+}
+function renderTask(task) {
+    //create HTML elements//
+    let item = document.createElement("li");
+    item.innerHTML = "<p>Task Name: " + task.taskDescription + "</p>" + "<p>" + 'Due Date:' + ' ' + task.dueDate + "</p>" + "<p>" + 'Completion Time:' + ' ' + task.completionTime + "</p>" + "<p>" + 'Estimated Completion Time:' + ' ' + task.estimatedTime + "</p>";
+    tasklist.appendChild(item);
+    //extra task DOM elements //
+    let delButton = document.createElement("button");
+    let delButtonText = document.createTextNode("Delete Task");
+    delButton.appendChild(delButtonText);
+    item.appendChild(delButton);
+    //event listners for DOM elements//
+    delButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        item.remove();
+    });
+    //clear the input form//
+    form.reset();
 }
 
 //# sourceMappingURL=index.f3bd186e.js.map
--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("taskform");
 const button = document.querySelector("#taskform > button")
 let taskInput = document.getElementById("taskInput");
+let tasklist = document.getElementById("tasklist");
 let dueDateInput = document.getElementById("dueDateInput");
 let completionTimeInput = document.getElementById("completionTimeInput");
 let estimatedTimeInput = document.getElementById("estimatedTimeInput");
@@ -15,7 +16,7 @@ addTask(task, dueDate, estimatedTime, completionTime, false);
 console.log(taskList);
 })
 
-let taskList = [];
+let taskListArray = [];
 
 function addTask(taskDescription, dueDate, estimatedTime, completionTime, completionStatus){
     let d = new Date();
@@ -26,10 +27,35 @@ function addTask(taskDescription, dueDate, estimatedTime, completionTime, comple
         estimatedTime,
         dateCreated,
         completionTime,
-        completionStatus,
-        
+        completionStatus, 
     };
 
-    taskList.push(task);
-    return task;
+    taskListArray.push(task);
+    renderTask(task);
+    }
+
+    function renderTask(task){
+//create HTML elements//
+    let item = document.createElement("li");
+    item.innerHTML = "<p>" + 'Task Name:' + ' ' + task.taskDescription + "</p>" + "<p>" + 'Due Date:'+ ' '+ task.dueDate + "</p>" + "<p>" + 'Completion Time:'+ ' '+ task.completionTime + "</p>" + "<p>" + 'Estimated Completion Time:'+ ' '+ task.estimatedTime + "</p>" ;
+
+    tasklist.appendChild(item)
+
+//extra task DOM elements //
+let delButton = document.createElement("button");
+let delButtonText = document.createTextNode("Delete Task");
+delButton.appendChild(delButtonText);
+item.appendChild(delButton);
+
+//event listners for DOM elements//
+delButton.addEventListener("click", function(event){
+    event.preventDefault();
+    item.remove();
+
+})
+
+
+//clear the input form//
+  form.reset();
+      
     }
